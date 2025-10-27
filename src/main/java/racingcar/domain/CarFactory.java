a2c54f6a8921d8dbdc0023d7d2bd5f50a44698a3
wldh25 <wldh25@hufs.ac.kr>
2025-10-27 21:51:47 +0900

    Feat : 자동차 2대 이상일때 경주 시작 가능

--- src/main/java/racingcar/domain/CarFactory.java
+++ src/main/java/racingcar/domain/CarFactory.java
@@ -6,7 +6,10 @@ public final class CarFactory {
     private CarFactory() {}
 
     public static Cars fromNames(List<CarName> names) {
-        List<Car> cars = names.stream()
+        if (names == null || names.size() < 2) {
+            throw new IllegalArgumentException("자동차는 최소 2대 이상이어야 합니다.");
+        }
+        var cars = names.stream()
                 .map(n -> new Car(n.value()))
                 .toList();
         return new Cars(cars);
